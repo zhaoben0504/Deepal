2d8ec54f0f05f8bf26abc7922c3750a3878a662a
zhaoben <zhaoben0504@163.com>
2023-12-04 21:08:16 +0800

    docs: main文件引入context

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"deepal"
-	"fmt"
 	"net/http"
 )
 
@@ -15,14 +14,20 @@ func main() {
 	//log.Fatal(http.ListenAndServe(":9999", engine))
 
 	r := deepal.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *deepal.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Deepal<h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *deepal.Context) {
+		// expect /hello?name=deepal
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *deepal.Context) {
+		c.JSON(http.StatusOK, deepal.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
